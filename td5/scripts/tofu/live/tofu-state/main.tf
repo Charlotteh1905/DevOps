ff11c7a116e1d3b97e5dabe258086ce8f1e98c8e
hernuc <charlotte.hernu@edu.esiee.fr>
2025-01-31 13:48:31 +0100

    Mise à jour de main.tf

--- td5/scripts/tofu/live/tofu-state/main.tf
+++ td5/scripts/tofu/live/tofu-state/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "state" {
-  source = "github.com/Charlotteh1905/DevOps//td5/tofu/modules/state-bucket"
+  source = "github.com/Charlotteh1905/DevOps//td5/scripts/tofu/live/tofu-state"
 
   # TODO: fill in your own bucket name!
   name = "charlotteh-tofu-state"
