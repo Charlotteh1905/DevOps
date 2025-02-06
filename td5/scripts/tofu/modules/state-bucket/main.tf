0a529d2d23aea2644fdea01b654a69e25431344b
hernuc <charlotte.hernu@edu.esiee.fr>
2025-02-06 20:51:37 +0100

    Ajout du workflow Tofu Plan

--- td5/scripts/tofu/modules/state-bucket/main.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  region = "us-east-2" # Your AWS region
-}
-module "state" {
-  source = "./"
-  name   = "YOUR-UNIQUE-BUCKET-NAME" # Replace with a unique name
-}
--- td5/scripts/tofu/modules/state-bucket/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "s3_bucket_name" {
-  value       = "charlotteh-tofu-state" # Nom exact du S3 bucket utilisé pour le backend
-  description = "The name of the S3 bucket used for Terraform state storage"
-}
-
-output "dynamodb_table_name" {
-  value       = "OpenTofuLockTable" # Nom exact de ta table DynamoDB utilisée pour les verrous
-  description = "The name of the DynamoDB table used for state locking"
-}
